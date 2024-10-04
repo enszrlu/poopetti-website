ba9de2075e37e006273125a7b6da2f489dcce019
enszrlu <65814876+enszrlu@users.noreply.github.com>
2024-10-04 16:49:26 +0100

    add great quote to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="py-6 mt-8">
-      <div className="container mx-auto text-center text-orange-600">
+    <footer className="py-6 mt-20">
+      <div className="container mx-auto text-center text-orange-600 flex flex-col gap-2">
         <p>© 2024 Poopetti. All rights reserved.</p>
+        <p className="italic text-sm">
+          Remember, with great poop comes great responsibility. Use <b>Poopetti</b>{' '}
+          wisely, and may your code always be as smooth as... well, you know. 💩✨
+        </p>
       </div>
     </footer>
   );
